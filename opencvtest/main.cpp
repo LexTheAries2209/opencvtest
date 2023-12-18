c0e1f8ae6297f6a384ff3e9ff39480e952d75245
LexTheAries2209 <673661658@qq.com>
2023-12-18 14:37:28 +0800

    Initial Commit

---
+++ opencvtest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  opencvtest
+//
+//  Created by 吴坤城 on 12/18/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
